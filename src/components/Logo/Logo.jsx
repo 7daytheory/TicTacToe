5a3ff23abb07dc77b8d326e96cf1a0ab27e4917b
Matthew Lowe <matthewjlowe11@gmail.com>
2024-10-16 22:02:38 -0400

    Found bug - instructions being rendered twice

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Banner from '../Banner/Banner';
 
 const Logo = () => {
+  const [showBanner, setShowBanner] = useState(false);
 
   const toggleInstructions = () => {
-    console.log("CLicked!");
-  }
+    setShowBanner((prev) => !prev);
+  };
 
   return (
-    <div className="bg-slate-800 text-white p-2 w-full flex justify-center">
-      <div className="flex justify-between items-center w-[900px] p-2">
-        <div className="text-2xl">
-          Tic Tac Toe
+    <div>
+      <div className="bg-slate-800 text-white p-2 w-full flex justify-center">
+        <div className="flex justify-between items-center w-[900px] p-2">
+          <div className="text-2xl">Tic Tac Toe</div>
+          <p className="cursor-pointer hover:text-gray-200" onClick={toggleInstructions}>
+            Instructions
+          </p>
         </div>
-        <p className='cursor-pointer hover:text-gray-200' onClick={toggleInstructions}>
-          Instructions
-        </p>
       </div>
+
+      {showBanner && <Banner />}
     </div>
   );
 };
