3f9dae3f2bc412a0497800c17143b61abc8d38a3
Matthew Lowe <matthewjlowe11@gmail.com>
2024-10-16 20:55:31 -0400

    Added instructions, the Instructions in the logo/nav will toggle them

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const Logo = () => {
+
+  const toggleInstructions = () => {
+    console.log("CLicked!");
+  }
+
   return (
     <div className="bg-slate-800 text-white p-2 w-full flex justify-center">
       <div className="flex justify-between items-center w-[900px] p-2">
         <div className="text-2xl">
           Tic Tac Toe
         </div>
-        <p className='pointer'>
+        <p className='cursor-pointer hover:text-gray-200' onClick={toggleInstructions}>
           Instructions
         </p>
       </div>
