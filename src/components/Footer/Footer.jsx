902f9df79d08ce6af665dbbcf329ffff3de825ce
Matthew Lowe <matthewjlowe11@gmail.com>
2024-10-18 17:22:19 -0400

    Add basic footer for spacing at bottom

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer class="bg-white">
+        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <hr class="my-6 border-gray-500 my-6" />
+            <span class="block text-sm text-gray-600 text-center">© 2023 <a href="https://matthewjlowe.com" class="hover:underline">Matthew Lowe</a>. All Rights Reserved.</span>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
