a6cd883b9d924667782d899d67ffeff0395bb1a0
Matthew Lowe <matthewjlowe11@gmail.com>
2024-10-18 16:15:24 -0400

    Show player 1 and player 2, depending whose turn it is

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -17,6 +17,9 @@ const Board = () => {
     // Switch between X and O based on the current player
     newSquares[i] = xIsNext ? 'X' : 'O';
 
+    // Update squares state
+    setSquares(newSquares);
+
     // Log the player who will play next
     console.log(`The next player will be using: ${xIsNext ? 'O' : 'X'}`);
 
@@ -35,46 +38,77 @@ const Board = () => {
     );
   };
 
+  const status = `Next player: ${xIsNext ? 'Player 1 (X)' : 'Player 2 (O)'}`;
+
   return (
-    <div className="flex flex-row justify-center items-center h-screen">
-      {/* Left Board */}
-      <div className="w-1/4 flex flex-col items-center justify-center bg-gray-200">
-        <div className="flex">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
+    <div className="flex flex-col items-center justify-center h-screen">
+      {/* Player 1 and Player 2 graphics */}
+      <div className="flex justify-between w-2/4 mb-8">
+        {/* Player 1 graphic */}
+        <div className={`text-2xl ${xIsNext ? 'text-blue-600 font-bold' : 'text-gray-400'}`}>
+          <div className="flex flex-col items-center mt-[85px]">
+            <p>Player 1</p>
+            <div className="w-20 h-20 bg-blue-600 text-white rounded-full flex items-center justify-center mt-2">
+              X
+            </div>
+          </div>
         </div>
-        <div className="flex">
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </div>
-        <div className="flex">
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
+
+        {/* Player 2 graphic */}
+        <div className={`text-2xl ${!xIsNext ? 'text-red-600 font-bold' : 'text-gray-400'}`}>
+          <div className="flex flex-col items-center mt-[85px]">
+            <p>Player 2</p>
+            <div className="w-20 h-20 bg-red-600 text-white rounded-full flex items-center justify-center mt-2 ">
+              O
+            </div>
+          </div>
         </div>
       </div>
 
-      {/* Middle Line */}
-      <div className="w-1 bg-black h-full"></div>
+      {/* Status message */}
+      <div className="mb-4 text-2xl font-bold">{status}</div>
 
-      {/* Right Board (Same as Left Board) */}
-      <div className="w-1/4 flex flex-col items-center justify-center bg-gray-200">
-        <div className="flex">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
+      {/* Game board */}
+      <div className="flex flex-row justify-center items-center">
+        {/* Left Board */}
+        <div className="w-1/4 flex flex-col items-center justify-center bg-gray-200">
+          <div className="flex">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="flex">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="flex">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
         </div>
-        <div className="flex">
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </div>
-        <div className="flex">
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
+
+        {/* Middle Line */}
+        <div className="w-1 bg-black h-full"></div>
+
+        {/* Right Board (Same as Left Board) */}
+        <div className="w-1/4 flex flex-col items-center justify-center bg-gray-200">
+          <div className="flex">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="flex">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="flex">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
         </div>
       </div>
     </div>
